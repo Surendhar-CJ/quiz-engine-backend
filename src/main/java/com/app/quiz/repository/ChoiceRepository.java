b33ea2dcb66ffb2ee777e8613a43145a2d394b0c
Surendhar CJ <surendharcjayapal@gmail.com>
2023-07-02 11:38:54 +0100

    Removed choice repository

--- src/main/java/com/app/quiz/repository/ChoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.quiz.repository;
-
-import com.app.quiz.entity.Choice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChoiceRepository extends JpaRepository<Choice, Long> {
-}
