e704d045b3a9f5c7806a257961822d26ad24cfbd
Surendhar CJ <surendharcjayapal@gmail.com>
2023-07-25 00:21:27 +0100

    Modified quiz null check

--- src/main/java/com/app/quiz/service/implementation/UserServiceImplementation.java
+++ src/main/java/com/app/quiz/service/implementation/UserServiceImplementation.java
@@ -65,7 +65,7 @@ public class UserServiceImplementation implements UserService {
         }
 
         List<QuizResult> userQuizzes = user.getQuizList().stream()
-                .filter(quiz -> quiz.getIsCompleted())
+                .filter(quiz -> quiz.getIsCompleted() && quiz.getCompletedAt() != null)
                 .map(quiz -> quizServiceImplementation.getQuizResult(quiz.getId()))
                 .toList();
 
