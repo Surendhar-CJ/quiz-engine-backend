0cd73e9815d452536040a9e549f03537740f3d88
Surendhar CJ <surendharcjayapal@gmail.com>
2023-08-11 10:58:29 +0100

    Configured JPQL for feedback and modified feedback exception message

--- src/main/java/com/app/quiz/repository/FeedbackContentRepository.java
+++ src/main/java/com/app/quiz/repository/FeedbackContentRepository.java
@@ -2,7 +2,11 @@ package com.app.quiz.repository;
 
 import com.app.quiz.entity.FeedbackContent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FeedbackContentRepository extends JpaRepository<FeedbackContent, Long> {
-    FeedbackContent findTopByMinScoreLessThanEqualAndMaxScoreGreaterThanEqual(Double minScore, Double maxScore);
+
+    @Query("SELECT feedback FROM FeedbackContent feedback" +
+            " WHERE feedback.minScore <= ?1 AND feedback.maxScore >= ?2")
+    FeedbackContent findByScoreRange(Double minScore, Double maxScore);
 }
